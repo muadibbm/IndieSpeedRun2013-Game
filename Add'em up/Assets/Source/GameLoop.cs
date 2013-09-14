63b62ab3abe5f5bbb15c2c48f742eb37a3d0e960
Mehrdad <muadibbm@HAL.(none)>
2013-09-14 13:46:29 -0400

    simple movement implemented

--- Add'em up/Assets/Source/GameLoop.cs
+++ Add'em up/Assets/Source/GameLoop.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameLoop : MonoBehaviour {
-
+	
+	public static Player player;
+	
 	void Start () 
 	{
-		
+		player = GameObject.Find ("Player").GetComponent<Player>();
+		player.init();
+		Time.fixedDeltaTime = 0.01f;
 	}
 	
 	void FixedUpdate () 
 	{
-		
+		player.updateStatus();
 	}
 }
---
+++ Add'em up/Assets/Source/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float jumping_height = 2.0f;
+	public float speed = 1.0f;
+	
+	private GameObject playerObject;
+	private float posZ;
+	private float posX;
+	private float posY;
+	private bool bJump;
+	private bool bMoveLeft;
+	private bool bMoveRight;
+	
+	public void init () 
+	{
+		playerObject = GameObject.Find ("Player");
+		posZ = playerObject.transform.position.z;
+		posX = playerObject.transform.position.x;
+		posY = playerObject.transform.position.y;
+		bJump = bMoveRight = bMoveLeft = false;
+	}
+	
+	public void updateStatus () 
+	{
+		updateInput();
+		posX = playerObject.transform.position.x;
+		posY = playerObject.transform.position.y;
+		if(bJump)
+			posY += jumping_height;
+		if(bMoveLeft)
+			posX -= speed;
+		if(bMoveRight)
+			posX += speed;
+		if(bJump || bMoveLeft || bMoveRight)
+			playerObject.transform.position = new Vector3(posX, posY, posZ);
+		if(bMoveLeft)
+			Debug.Log ("left");
+		if(bMoveRight)
+			Debug.Log ("Right");
+		if(bJump)
+			Debug.Log("jump");
+	}
+	
+	private void updateInput()
+	{
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+			bJump = true;
+		if(Input.GetKeyUp(KeyCode.UpArrow))
+			bJump = false;
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+			bMoveLeft = true;
+		if(Input.GetKeyUp(KeyCode.LeftArrow))
+			bMoveLeft = false;
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+			bMoveRight = true;
+		if(Input.GetKeyUp(KeyCode.RightArrow))
+			bMoveRight = false;
+	}
+}
