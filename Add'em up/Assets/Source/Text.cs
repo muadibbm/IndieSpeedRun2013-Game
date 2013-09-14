630dd82653fd8851d3f9fd247ffcbc60065458ae
Mehrdad <muadibbm@HAL.(none)>
2013-09-14 18:55:32 -0400

    text and text_trigger implemeted

---
+++ Add'em up/Assets/Source/Text.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Text : MonoBehaviour {
+	
+	public int fadeValue = 100;
+	
+	private int fadeCounter = 255;
+	private Color color;
+	private bool bFade = false;
+	
+	void Start()
+	{
+		color = renderer.material.color;	
+	}
+	
+	void FixedUpdate()
+	{
+		if(bFade)
+		{
+			color.a = fadeCounter;
+			renderer.material.color = color;
+			fadeCounter--;
+			if(fadeCounter == fadeValue)
+				collider.enabled = false;
+			if(fadeCounter <= 0)
+			{
+				renderer.enabled = false;
+				return;
+			}
+		}
+		Debug.Log (fadeCounter);
+	}
+	
+	public void fade () 
+	{
+		bFade = true;
+	}
+}
---
+++ Add'em up/Assets/Source/Text_Trigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Text_Trigger : MonoBehaviour {
+	
+	private bool bTouched = false;
+	public GameObject refObject;
+	
+	void OnTriggerEnter(Collider other) 
+	{
+        if(other.Equals(GameObject.Find("Player").collider))
+		{
+			bTouched = true;
+			refObject.GetComponent<Text>().fade();
+		}
+    }
+}
